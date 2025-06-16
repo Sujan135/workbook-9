71d22aeaa93b300f1eec7c90e726a2ff95e44b60
Sujan Banjara <banjara.suzan12@gmail.com>
2025-06-16 13:29:59 -0500

    Add product menu to list and add products using DAO

--- NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
+++ NorthwindTradersSpringBoot/src/main/java/com/pluralsight/NorthwindTradersSpringBoot/NorthwindTradersSpringBootApplication.java
@@ -2,12 +2,14 @@ package com.pluralsight.NorthwindTradersSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class NorthwindTradersSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(NorthwindTradersSpringBootApplication.class, args);
+		ProductRunnerMenu menu = context.getBean(ProductRunnerMenu.class);
+		menu.startMenu();
 	}
-
 }
